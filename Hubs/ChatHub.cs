06e5333ffcdba4b9a71e7a1d6a3d5964efe9604c
Aranold1 <henry_lav@hotmail.com>
2024-03-11 10:50:57 +0700

    small fixes

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -12,79 +12,55 @@ public class ChatHub : Hub
     {
         PropertyNameCaseInsensitive = true,
     };
-    public async Task SendMessage(string message,int? chatId,int userId)
+    public async Task SendMessage(string message, int? chatId, int userId)
     {
-            using(var client = new HttpClient())
+        using (var client = new HttpClient())
+        {
+
+            if (chatId is null)
             {
-                #if DEBUG
-                if (chatId is null)
+                try
                 {
-                    try
-                    {
-                        HttpResponseMessage responseMessage =
+#if DEBUG
+                    HttpResponseMessage responseMessage =
                             await client.GetAsync($"http://localhost:5001/createnewchat?message={message}&userId={userId}");
-
-                        var answer = await responseMessage.Content.ReadAsStringAsync();
-                        await Clients.Caller.SendAsync("ReciveAnswer", answer);
-                    }
-                    catch
-                    {
-                        await Clients.Caller.SendAsync("ReciveMessage", "Что то пошло не так...");
-                    }
-                }
-                else
-                {
-                    try
-                    {
-                        HttpResponseMessage responseMessage =
-                            await client.GetAsync($"http://localhost:5001/addmessagestochat?chatId={chatId}&message={message}");
-
-                        var answer = await responseMessage.Content.ReadAsStringAsync();
-                        await Clients.Caller.SendAsync("ReciveAnswer", answer);
-                    }
-                    catch
-                    {
-                        await Clients.Caller.SendAsync("ReciveMessage", "Что то пошло не так...");
-
-                    }
-                }
-#endif
-
-#if (!DEBUG)
-                if (chatId is null)
-                {
-                    try
-                    {
-                        HttpResponseMessage responseMessage =
+#else
+                    HttpResponseMessage responseMessage =
                             await client.GetAsync($"http://apiforblazor:5001/createnewchat?message={message}&userId={userId}");
-
-                        var answer = await responseMessage.Content.ReadAsStringAsync();
-                        await Clients.Caller.SendAsync("ReciveAnswer", answer);
-                    }
-                    catch
-                    {
-                        await Clients.Caller.SendAsync("ReciveMessage", "Что то пошло не так...");
-                    }
-                }
-                else
-                {
-                    try
-                    {
-                        HttpResponseMessage responseMessage =
-                            await client.GetAsync($"http://apiforblazor:5001/addmessagestochat?chatId={chatId}&message={message}");
-
-                        var answer = await responseMessage.Content.ReadAsStringAsync();
-                        await Clients.Caller.SendAsync("ReciveAnswer", answer);
-                    }
-                    catch
-                    {
-                        await Clients.Caller.SendAsync("ReciveMessage", "Что то пошло не так...");
-                    }
-                }
 #endif
+                    var answer = await responseMessage.Content.ReadAsStringAsync();
+                    await Clients.Caller.SendAsync("ReciveAnswer", answer);
+                }
+                catch
+                {
+                    await Clients.Caller.SendAsync("ReciveMessage", "что то пошло не так...");
+                }
+            }
+            else
+            {
+                try
+                {
+#if DEBUG
+                    HttpResponseMessage responseMessage =
+                            await client.GetAsync($"http://localhost:5001/addmessagestochat?chatId={chatId}&message={message}");
+#else
+                    HttpResponseMessage responseMessage =
+                            await client.GetAsync($"http://apiforblazor:5001/addmessagestochat?chatId={chatId}&message={message}");
+#endif
+                    var answer = await responseMessage.Content.ReadAsStringAsync();
+                    await Clients.Caller.SendAsync("ReciveAnswer", answer);
+                }
+                catch
+                {
+
+                }
+            }
+
+
+
 
         }
 
     }
-	
+
 }
